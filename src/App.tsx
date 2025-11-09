69752776a94598a62f9ac9cbd9f8e0f14b00c2d3
Jefone Gorme <jefonegorme@gmail.com>
2025-11-09 20:12:42 +0800

    Temporary: White page when accessed by large devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Hero from './components/Hero/Hero';
 // import Stack from './components/Stack/Stack';
+
+//temporary
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
 
 /**
  * The `App` component to be rendered.
@@ -8,6 +32,12 @@ import Hero from './components/Hero/Hero';
  * @returns   {JSX.Element}   The `App` component.
  */
 function App() {
+  const size = useWindowSize();
+
+  if (810 < size.width) {
+    return <div>Please use mobile</div>
+  }
+
   return (
     <div className="App">
       <div className="container-box">
